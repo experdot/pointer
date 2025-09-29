fa07e7bce228c4226b9d9d22ac49589a3fcd86d8
experdot <experdot@foxmail.com>
2025-09-29 21:45:31 +0800

    feat: Refactor SidebarActions to use Dropdown for chat creation options, enhancing UI and grouping related actions

--- src/renderer/src/components/layout/sidebar_items/SidebarActions.tsx
+++ src/renderer/src/components/layout/sidebar_items/SidebarActions.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Button, Space, Tooltip } from 'antd'
+import { Button, Space, Tooltip, Dropdown } from 'antd'
+import type { MenuProps } from 'antd'
 import {
   PlusOutlined,
   FolderAddOutlined,
   DeleteOutlined,
   TableOutlined,
-  BlockOutlined
+  BlockOutlined,
+  DownOutlined
 } from '@ant-design/icons'
 
 interface SidebarActionsProps {
@@ -27,21 +29,39 @@ export default function SidebarActions({
   onCreateFolder,
   onBatchDelete
 }: SidebarActionsProps) {
+  const createOptions: MenuProps['items'] = [
+    {
+      key: 'crosstab',
+      label: '新建交叉视图',
+      icon: <TableOutlined />,
+      onClick: onCreateCrosstabChat
+    },
+    {
+      key: 'object',
+      label: '新建对象页面',
+      icon: <BlockOutlined />,
+      onClick: onCreateObjectChat
+    },
+    {
+      key: 'folder',
+      label: '新建文件夹',
+      icon: <FolderAddOutlined />,
+      onClick: onCreateFolder
+    }
+  ]
   if (collapsed) {
     return (
       <div className="sidebar-actions-collapsed">
-        <Tooltip title="新建聊天" placement="right">
-          <Button type="text" icon={<PlusOutlined />} onClick={onCreateChat} />
-        </Tooltip>
-        <Tooltip title="新建交叉视图" placement="right">
-          <Button type="text" icon={<TableOutlined />} onClick={onCreateCrosstabChat} />
-        </Tooltip>
-        <Tooltip title="新建对象页面" placement="right">
-          <Button type="text" icon={<BlockOutlined />} onClick={onCreateObjectChat} />
-        </Tooltip>
-        <Tooltip title="新建文件夹" placement="right">
-          <Button type="text" icon={<FolderAddOutlined />} onClick={onCreateFolder} />
-        </Tooltip>
+        <Button.Group>
+          <Tooltip title="新建聊天" placement="right">
+            <Button type="text" icon={<PlusOutlined />} onClick={onCreateChat} />
+          </Tooltip>
+          <Tooltip title="更多选项" placement="right">
+            <Dropdown menu={{ items: createOptions }} trigger={['click']}>
+              <Button type="text" icon={<DownOutlined />} />
+            </Dropdown>
+          </Tooltip>
+        </Button.Group>
         {hasCheckedItems && (
           <Tooltip title="批量删除" placement="right">
             <Button
@@ -60,18 +80,18 @@ export default function SidebarActions({
   return (
     <div className="sidebar-actions">
       <Space>
-        <Tooltip title="新建聊天">
-          <Button type="text" icon={<PlusOutlined />} onClick={onCreateChat} />
-        </Tooltip>
-        <Tooltip title="新建交叉视图">
-          <Button type="text" icon={<TableOutlined />} onClick={onCreateCrosstabChat} />
-        </Tooltip>
-        <Tooltip title="新建对象页面">
-          <Button type="text" icon={<BlockOutlined />} onClick={onCreateObjectChat} />
-        </Tooltip>
-        <Tooltip title="新建文件夹">
-          <Button type="text" icon={<FolderAddOutlined />} onClick={onCreateFolder} />
-        </Tooltip>
+        <Button.Group>
+          <Tooltip title="新建聊天">
+            <Button type="text" icon={<PlusOutlined />} onClick={onCreateChat}>
+              新建聊天
+            </Button>
+          </Tooltip>
+          <Tooltip title="更多选项">
+            <Dropdown menu={{ items: createOptions }} trigger={['click']}>
+              <Button type="text" icon={<DownOutlined />} />
+            </Dropdown>
+          </Tooltip>
+        </Button.Group>
         {hasCheckedItems && (
           <Tooltip title="批量删除">
             <Button
