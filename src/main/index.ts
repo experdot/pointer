30e8a7930b05617f723800823a02b1b4c4b6965d
experdot <experdot@foxmail.com>
2025-11-21 20:23:09 +0800

    feat: update app user model ID and enhance package description

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,7 +11,7 @@ import { createWindow } from './window'
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(async () => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.experdot.pointer')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
